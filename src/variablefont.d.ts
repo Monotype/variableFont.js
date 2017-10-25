39c699c0e86817ae51a3a9986595f3874907d948
nrip-monotype <nrip.nath@monotype.com>
2017-10-25 14:07:00 +0545

    OOP VariableFont class.
    The VariableFont class wraps opentype font object.

--- src/variablefont.d.ts
+++ src/variablefont.d.ts
@@ -1,32 +1,22 @@
-interface IFontAxis {
-    tag: string,
-    min: number,
-    max: number,
-    default: number,
-    name: string
+interface IFontTable {
+    [name: string]: any;
 }
 
-interface IFontTable {
-    fvar: {
-        axes: IFontAxis[],
-        instances: any[],
-    }
-}
 interface IFontInfo {
     url: string | null,
-    tables: IFontTable,
-    axes: IFontAxis[],
-    instances: any[],
-}
-
-interface IFontType {
-    fontFamily: string,
-    menuName: string,
-    axes: IFontAxis[],
-    instances: any[],
     names: any,
-    type: string,
-    url: string
+    tables: IFontTable,
+    ascender: number,
+    descender: number,
+    encoding: any,
+    glyphNames: any,
+    glyphs: any,
+    kerningPairs: any,
+    numGlyphs: number,
+    numberOfHMetrics: number,
+    outlinesFormat: string
+    substitution: any,
+    unitsPerEm: number
 }
 
 declare var opentype: {
